86dc5db8a035ed41b8a5140f38077c52b9f7f12a
this-is-shams <shamsfarabi1998@gmail.com>
2024-03-12 12:41:02 +0600

    first commit

---
+++ schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+
+class NoteModel(BaseModel):
+    id: str
+    title: str
+    content: str
+    date_created : datetime
+    
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+    
+    
+class NoteCreateModel(BaseModel):
+    title: str
+    content: str
+    
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_schema_extra={
+            "example" : {
+                "title" : "Sample Title",
+                "content": "Sample Content"
+            }
+        }
+    )
